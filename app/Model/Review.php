676a0663cc68ae47eea0adf7f29080b689d738fa
sudipsingh04 <sudipsingh04@gmail.com>
2020-04-19 15:10:32 +0530

    Review Create api

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+    protected $guarded = ['id'];
+
     public function product()
     {
         return $this->belongsTo(Product::class);
